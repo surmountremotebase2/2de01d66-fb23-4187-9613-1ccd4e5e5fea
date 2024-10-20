9eebc0c0520f9d13996ca589e7207fabd5c56a3d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 19:43:39 -0400

    User Faye Roske saved changes to multiple files files at 10/20/2024, 23:43:39 UTC time.

--- 0022f11d-3615-405b-86a0-4589a223ceb3/main.py
+++ 0022f11d-3615-405b-86a0-4589a223ceb3/main.py
@@ -31,13 +31,13 @@ class TradingStrategy(Strategy):
             last_close_price = rxrx_data[-1]["RXRX"]["close"]  # Get the latest closing price
             prev_close_price = rxrx_data[-2]["RXRX"]["close"]  # Get the previous closing price
 
-            # Buy condition: if the MACD line crosses above the signal line
-            if macd_line[-2] < signal_line[-2] and macd_line[-1] > signal_line[-1]:
+            # Buy condition: if the MACD line crosses below the signal line
+            if macd_line[-2] > signal_line[-2] and macd_line[-1] < signal_line[-1]:
                 log("Buying signal triggered")
                 allocation = 1  # Full allocation
             
             # Sell condition: if the MACD line crosses above the signal line
-            elif macd_line[-2] > signal_line[-2] and macd_line[-1] < signal_line[-1]:
+            elif macd_line[-2] < signal_line[-2] and macd_line[-1] > signal_line[-1]:
                 log("Selling signal triggered")
                 allocation = 0  # No allocation due to selling
         
