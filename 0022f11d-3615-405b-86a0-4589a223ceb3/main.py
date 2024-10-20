cc8a2c23d636d44ca46c2f5c00c9386b43aff7ab
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 19:29:15 -0400

    User Faye Roske saved changes to multiple files files at 10/20/2024, 23:29:14 UTC time.

--- 0022f11d-3615-405b-86a0-4589a223ceb3/main.py
+++ 0022f11d-3615-405b-86a0-4589a223ceb3/main.py
@@ -4,7 +4,7 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["RXRX"]  # Define the ticker we are interested in
+        self.tickers = ["RKLB"]  # Define the ticker we are interested in
 
     @property
     def interval(self):
