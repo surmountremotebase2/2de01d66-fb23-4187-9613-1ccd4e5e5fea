e6c5d35a0db5267a07e7f7570640a197bd1cdea8
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 13:52:54 -0400

    User Faye Roske saved changes to main.py at 10/20/2024, 17:52:53 UTC time.

--- 9dcc21f5-8a8b-4751-8a07-af6524c58abf/main.py
+++ 9dcc21f5-8a8b-4751-8a07-af6524c58abf/main.py
@@ -1,5 +1,5 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import SMA
+from surmount.technical_indicators import SMA, EMA
 from surmount.logging import log
 
 class TradingStrategy(Strategy):
@@ -21,24 +21,24 @@ class TradingStrategy(Strategy):
         ticker = self.tickers[0]  # Assuming only one ticker is used in this strategy
         ohlcv_data = data["ohlcv"]
         
-        if len(ohlcv_data) > 20:  # Ensures there's enough data to calculate SMA
-            sma20 = SMA(ticker, ohlcv_data, length=2)  # Calculates 20 periods SMA
+        if len(ohlcv_data) > 20:  # Ensures there's enough data to calculate EMA
+            ema20 = EMA(ticker, ohlcv_data, length=2)  # Calculates 20 periods EMA
             
             current_price = ohlcv_data[-1][ticker]["close"]
-            last_sma_value = sma20[-1]
+            last_ema_value = ema20[-1]
             
-            if current_price - last_sma_value <= -0.01:
-                # Current price is below the 20-day SMA, indicating a potential buy signal
-                log(f"Buying signal for {ticker}: current price {current_price} is lower than 20-day SMA of {last_sma_value}.")
+            if current_price - last_ema_value <= -0.01:
+                # Current price is below the 20-day EMA, indicating a potential buy signal
+                log(f"Buying signal for {ticker}: current price {current_price} is lower than 20-day EMA of {last_ema_value}.")
                 allocation = 1  # Sets allocation to 100%
-            elif current_price - last_sma_value >= 0.03:
-                # Current price is above the 20-day SMA, indicating a potential sell signal
-                log(f"Selling signal for {ticker}: current price {current_price} is higher than 20-day SMA of {last_sma_value}.")
+            elif current_price - last_ema_value >= 0.03:
+                # Current price is above the 20-day EMA, indicating a potential sell signal
+                log(f"Selling signal for {ticker}: current price {current_price} is higher than 20-day EMA of {last_ema_value}.")
                 allocation = 0  # Sets allocation to 0%
             else:
-                allocation = 0  # Default allocation in case the price equals the SMA
+                allocation = 0  # Default allocation in case the price equals the EMA
         else:
-            log("Not enough data to calculate SMA.")
+            log("Not enough data to calculate EMA.")
             allocation = 0  # Default allocation if not enough data
 
         return TargetAllocation({ticker: allocation})
