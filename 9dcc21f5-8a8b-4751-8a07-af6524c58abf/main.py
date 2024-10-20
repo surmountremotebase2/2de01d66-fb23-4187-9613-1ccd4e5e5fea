d2c67a886b8c20fb5ed26e875cdf9c8440f3fc9e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 13:33:41 -0400

    User Faye Roske saved changes to main.py at 10/20/2024, 17:33:40 UTC time.

--- 9dcc21f5-8a8b-4751-8a07-af6524c58abf/main.py
+++ 9dcc21f5-8a8b-4751-8a07-af6524c58abf/main.py
@@ -4,7 +4,7 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["LUNR"]
+        self.tickers = ["RKLB"]
 
     @property
     def interval(self):
