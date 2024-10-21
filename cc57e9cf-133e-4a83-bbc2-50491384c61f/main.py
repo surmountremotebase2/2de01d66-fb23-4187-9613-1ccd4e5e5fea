cab89385a6283b89d712f4646ffe80f07c41814e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 20:54:43 -0400

    User Faye Roske saved changes to multiple files files at 10/21/2024, 00:54:42 UTC time.

--- cc57e9cf-133e-4a83-bbc2-50491384c61f/main.py
+++ cc57e9cf-133e-4a83-bbc2-50491384c61f/main.py
@@ -6,7 +6,7 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
     def __init__(self):
         self.ticker = "CLOV"
-        self.previous_price = 0
+        self.previous_price = None
 
         
     @property
