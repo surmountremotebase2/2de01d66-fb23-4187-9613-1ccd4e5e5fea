55c51b5dc1e49c46b64938ba95ada6fb12a9eef9
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 20:18:19 -0400

    User Faye Roske saved changes to main.py at 10/21/2024, 00:18:18 UTC time.

--- cc57e9cf-133e-4a83-bbc2-50491384c61f/main.py
+++ cc57e9cf-133e-4a83-bbc2-50491384c61f/main.py
@@ -27,7 +27,10 @@ class TradingStrategy(Strategy):
         current_price = clov_data[-1][self.ticker]["close"]
         
         # Calculate the 5-minute SMA for CLOV. Length is set to 5 for the 5-minute SMA.
+        clov_sma_10min = SMA(self.ticker, clov_data, 10)
         clov_sma_5min = SMA(self.ticker, clov_data, 5)
+        clov_sma_3min = SMA(self.ticker, clov_data, 3)
+        clov_sma_1min = SMA(self.ticker, clov_data, 1)
         
         if len(clov_sma_5min) == 0:
             # If we do not have enough data to calculate SMA, we do not return any allocation
@@ -38,11 +41,11 @@ class TradingStrategy(Strategy):
         
         allocation = 0
         
-        if current_price < sma_5min_current:
-            # Current price is below the 5-minute SMA, setting allocation to buy (1)
+        if current_price < clov_sma_1_min:
+            # Current price is below the 1-minute SMA, setting allocation to buy (1)
             log(f'Current price of {self.ticker} below 5min SMA, buying')
             allocation = 1
-        elif current_price > sma_5min_current + 0.03:
+        elif current_price > sma_10min_current + 0.03:
             # Current price is above the 5-minute SMA by more than 3 cents, setting allocation to sell (0)
             log(f'Current price of {self.ticker} above 5min SMA by more than 0.03, selling')
             allocation = 0
