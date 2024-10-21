b8800a9a46b664786b7d1caa4069a74ff7d81d7b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-10-20 20:35:57 -0400

    User Faye Roske saved changes to multiple files files at 10/21/2024, 00:35:56 UTC time.

--- cc57e9cf-133e-4a83-bbc2-50491384c61f/main.py
+++ cc57e9cf-133e-4a83-bbc2-50491384c61f/main.py
@@ -6,6 +6,8 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
     def __init__(self):
         self.ticker = "CLOV"
+        self.previous_price = None
+
         
     @property
     def assets(self):
@@ -25,6 +27,9 @@ class TradingStrategy(Strategy):
         # Access the latest minute's close price data for CLOV
         clov_data = data["ohlcv"]
         current_price = clov_data[-1][self.ticker]["close"]
+
+        if self.previous_price is not None:
+           price_change = current_price - self.previous_price
         
         # Calculate the 5-minute SMA for CLOV. Length is set to 5 for the 5-minute SMA.
         clov_sma_10min = SMA(self.ticker, clov_data, 10)
@@ -37,20 +42,41 @@ class TradingStrategy(Strategy):
             return TargetAllocation({})
         
         # The last value from clov_sma_5min gives us the latest SMA value
+        sma_10_min_current = clov_sma_10min[-1]
         sma_5min_current = clov_sma_5min[-1]
+        sma_3min_current = clov_sma_3min[-1]
+        sma_1min_current = clov_sma_1min[-1]
         
         allocation = 0
+
+        price_difference = current_price - previous_price
+        log(f"current price: {current_price}; previous price: {previous_price}; difference: {price_difference}")
+
         
-        if current_price < clov_sma_1_min:
-            # Current price is below the 1-minute SMA, setting allocation to buy (1)
-            log(f'Current price of {self.ticker} below 5min SMA, buying')
+        # TODO: combine current price v previous price with sma
+        if -0.01 > price_difference > 0:
+            allocation = 0.55
+        
+        elif -.02 > price_difference => -0.01: 
+            allocation = 0.6
+
+        elif -.03 > price_difference => -0.05: 
+            allocation = 0.75
+
+        elif -.05 > price_difference => -0.10: 
+            allocation = 0.80
+
+        elif -.10 > price_difference: 
             allocation = 1
-        elif current_price > sma_10min_current + 0.03:
-            # Current price is above the 5-minute SMA by more than 3 cents, setting allocation to sell (0)
-            log(f'Current price of {self.ticker} above 5min SMA by more than 0.03, selling')
+
+        elif price_difference > 0 and current_price > sma_3min_current
             allocation = 0
+
         else:
-            log(f'{self.ticker} price is within threshold of 5min SMA, holding position')
+            log(f'{self.ticker} no conditions met; holding position')
             
+        # Update the previous price with the current price for the next run.
+        self.previous_price = current_price
+
         # Returning the target allocation based on the logic above
         return TargetAllocation({self.ticker: allocation})
